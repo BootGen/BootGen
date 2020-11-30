5e056006ca49eea3e9caee48880543fe567960fa
Orsós Lívia <orsoslivia@hotmail.com>
2020-11-30 12:28:46 +0100

    highlightjs

--- WebProject/ClientApp/src/main.ts
+++ WebProject/ClientApp/src/main.ts
@@ -4,7 +4,10 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import { User } from './models/User'
+import hljs from 'highlight.js';
+import 'highlight.js/styles/vs.css';
 
+Vue.use(hljs.vuePlugin);
 Vue.config.productionTip = false
 
 store.commit('init');
