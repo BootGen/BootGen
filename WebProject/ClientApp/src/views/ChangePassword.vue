19b030c1e17968efc081e5321d6a87bc93620db7
Orsós Lívia <orsoslivia@hotmail.com>
2020-12-09 17:22:56 +0100

    change password

--- WebProject/ClientApp/src/views/ChangePassword.vue
+++ WebProject/ClientApp/src/views/ChangePassword.vue
@@ -1,48 +1,93 @@
 <template>
-  <v-container fluid>
-    <v-row>
-      <v-col cols="8">
-        <ValidationObserver v-slot="{ invalid }">
-          <ValidationProvider
-            v-slot="{ errors }"
-            name="password"
-            rules="required"
-          >
-            <v-text-field
-              v-model="oldPassword"
-              :error-messages="errors"
-              placeholder="old password"
-              type="password"
-            ></v-text-field>
-          </ValidationProvider>
-          <ValidationProvider
-            v-slot="{ errors }"
-            name="password"
-            rules="required|min:8|password:@confirmation"
-          >
-            <v-text-field
-              v-model="newPassword"
-              :error-messages="errors"
-              placeholder="new password"
-              type="password"
-            ></v-text-field>
-          </ValidationProvider>
-          <ValidationProvider
-            v-slot="{ errors }"
-            name="confirmation"
-            rules="required|min:8"
-          >
-            <v-text-field
-              v-model="confirmPassword"
-              :error-messages="errors"
-              placeholder="confirm password"
-              type="password"
-            ></v-text-field>
-          </ValidationProvider>
-          <v-alert type="error" v-if="errorMsg">{{ errorMsg }}</v-alert>
-          <v-alert type="success" v-if="successMsg">{{ successMsg }}</v-alert>
-          <v-btn large @click="savePassword" :disabled="invalid">Save</v-btn>
-        </ValidationObserver>
+  <v-container id="user-profile" fluid tag="section">
+    <v-row justify="center" v-if="$root.$data.user">
+      <v-col cols="12" md="8">
+        <base-material-card>
+          <template v-slot:heading>
+            <div class="display-2 font-weight-light">
+              Change Password
+            </div>
+
+            <div class="subtitle-1 font-weight-light">
+              Complete your password
+            </div>
+          </template>
+
+          <v-form>
+            <v-container class="py-0">
+              <v-row>
+                <v-col cols="12">
+                  <ValidationObserver v-slot="{ invalid }">
+                    <ValidationProvider
+                      v-slot="{ errors }"
+                      name="password"
+                      rules="required"
+                    >
+                      <v-text-field
+                        v-model="oldPassword"
+                        :error-messages="errors"
+                        placeholder="old password"
+                        type="password"
+                      ></v-text-field>
+                    </ValidationProvider>
+                    <ValidationProvider
+                      v-slot="{ errors }"
+                      name="password"
+                      rules="required|min:8|password:@confirmation"
+                    >
+                      <v-text-field
+                        v-model="newPassword"
+                        :error-messages="errors"
+                        placeholder="new password"
+                        type="password"
+                      ></v-text-field>
+                    </ValidationProvider>
+                    <ValidationProvider
+                      v-slot="{ errors }"
+                      name="confirmation"
+                      rules="required|min:8"
+                    >
+                      <v-text-field
+                        v-model="confirmPassword"
+                        :error-messages="errors"
+                        placeholder="confirm password"
+                        type="password"
+                      ></v-text-field>
+                    </ValidationProvider>
+                    <v-alert type="error" v-if="errorMsg">{{ errorMsg }}</v-alert>
+                    <v-alert type="success" v-if="successMsg">{{ successMsg }}</v-alert>
+                    <v-col cols="12" class="text-right">
+                      <v-btn color="success" class="mr-0"  @click="savePassword" :disabled="invalid">
+                        Update Profile
+                      </v-btn>
+                    </v-col>
+                  </ValidationObserver>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-form>
+        </base-material-card>
+      </v-col>
+
+      <v-col cols="12" md="4">
+        <base-material-card
+          class="v-card-profile"
+          avatar="https://demos.creative-tim.com/vue-material-dashboard/img/marc.aba54d65.jpg"
+        >
+          <v-card-text class="text-center">
+            <h4 class="display-2 font-weight-light mb-3 black--text">
+              {{ $root.$data.user.userName }}
+            </h4>
+
+            <p class="font-weight-light grey--text">
+              {{ $root.$data.user.email }}
+            </p>
+
+            <v-btn color="success" rounded class="mr-0" to="/profile">
+              Change Profile Data
+            </v-btn>
+          </v-card-text>
+        </base-material-card>
       </v-col>
     </v-row>
   </v-container>
