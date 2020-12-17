638c75f12e91bbe4652d5b94655d4d3b51b1d2c2
Orsós Lívia <orsoslivia@hotmail.com>
2020-12-17 12:46:59 +0100

    help dialog

---
+++ WebProject/ClientApp/src/components/HelpDialog.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-dialog v-model="dialog">
+    <template v-slot:activator="{ on, attrs }">
+			<v-btn class="ml-2" color="white" elevation="1" fab small v-bind="attrs" v-on="on">
+				<v-icon color="primary">mdi-help</v-icon>
+			</v-btn>
+    </template>
+    <v-card>
+      <v-toolbar flat color="primary" dark>
+				<v-btn icon dark @click="dialog = false">
+					<v-icon>mdi-close</v-icon>
+				</v-btn>
+        <v-toolbar-title>Help</v-toolbar-title>
+      </v-toolbar>
+      <v-tabs>
+        <v-tab v-for="item in help" :key="item.title">
+          <v-icon left>
+            {{item.icon}}
+          </v-icon>
+          {{item.title}}
+        </v-tab>
+        <v-tab-item v-for="item in help" :key="item.title">
+          <v-card flat>
+            <v-card-text>
+              <p>
+               {{item.description}}
+              </p>
+            </v-card-text>
+          </v-card>
+        </v-tab-item>
+      </v-tabs>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  components: {
+  },
+  data: function () {
+    return {
+      dialog: false,
+      help: [
+        {title: "Title 1", icon: "mdi-account", description: "Description 1"}, 
+        {title: "Title 2", icon: "mdi-clock", description: "Description 2"}, 
+        {title: "Title 3", icon: "mdi-cog", description: "Description 3"}, 
+      ],
+    };
+  },
+  methods: {
+  },
+});
+</script>
--- WebProject/ClientApp/src/components/Options.vue
+++ WebProject/ClientApp/src/components/Options.vue
@@ -1,13 +1,15 @@
 <template>
-  <v-container fluid>
+  <v-container fluid class="pa-0">
 		<v-row v-if="$store.state.projects.length < 1" class="d-flex justify-end">
 			<p class="mr-4">for save <a href="/">sign in</a></p>
 			<v-btn class="mr-4" color="primary" small @click="download">Download</v-btn>
+			<help-dialog></help-dialog>
     </v-row>
 		<v-row v-else class="d-flex align-center">
 			<v-select class="mr-4" label="My Projects" @change="selectProject" :items="$store.state.projects" item-text="name" return-object></v-select>
 			<save-project-dialog :project="project"></save-project-dialog>
 			<v-btn class="mr-4" color="primary" small @click="download">Download</v-btn>
+			<help-dialog></help-dialog>
     </v-row>
   </v-container>
 </template>
@@ -17,13 +19,15 @@ import Vue from "vue";
 import { Project } from "../models/Project";
 import { User } from "../models/User";
 import SaveProjectDialog from "../components/SaveProjectDialog.vue";
+import HelpDialog from "../components/HelpDialog.vue";
 
 export default Vue.extend({
 	props:[
 		"project"
 	],
   components: {
-    SaveProjectDialog,
+		SaveProjectDialog,
+		HelpDialog,
   },
 	created: async function() {
 		if(this.$store.state.jwt){
