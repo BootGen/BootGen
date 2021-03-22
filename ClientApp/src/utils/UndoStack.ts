be8ec8c8b763be96af2d91ed9c94bec7e273face
Gabor Angyal <angyalgabor@outlook.com>
2021-03-22 19:36:07 +0100

    undo stack

---
+++ ClientApp/src/utils/UndoStack.ts
@@ -0,0 +1,25 @@
+
+import { CRC32 } from 'crc_32_ts';
+
+export interface UndoStackFrame {
+    crc32: number;
+    content: string;
+}
+
+export class UndoStack {
+    private data = Array<UndoStackFrame>();
+    public top(): UndoStackFrame|null {
+        if (this.data.length == 0)
+            return null;
+        return this.data[this.data.length-1];
+    }
+    public pop() {
+        this.data.pop();
+    }
+    public push(content: string) {
+        this.data.push({
+            content: content,
+            crc32: CRC32.str(content)
+        });
+    }
+}
