afdc855a0b605686ad2d53f290d512e8d56ceee6
Gabor Angyal <angyalgabor@outlook.com>
2021-04-16 21:33:50 +0200

    use data files in vue unit tests

--- ClientApp/tests/unit/diff.spec.ts
+++ ClientApp/tests/unit/diff.spec.ts
@@ -1,12 +1,18 @@
 import { expect } from 'chai'
 import { getChanges } from '@/utils/TextCompare';
+import * as fs from 'fs';
+
+function read(fileName: string): string {
+  return fs.readFileSync(`./tests/unit/${fileName}`, 'utf8');
+}
+
 
 describe('TextCompare', function() {
   describe('getChanges', function() {
-    it('smoke test', function() {
-      const changes = getChanges('a', 'b');
+    it('should compare two C# files', function() {
+      const changes = getChanges(read('Tag.cs(1)'), read('Tag.cs(2)'));
       expect(changes.length).to.equal(1);
-      expect(changes[0]).to.equal(0);
+      expect(changes[0]).to.equal(8);
     });
   });
 });
