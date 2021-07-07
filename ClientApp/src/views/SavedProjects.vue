2b571c061503257ef9427462a219cf66e1af39d8
Orsós Lívia <orsoslivia@hotmail.com>
2021-07-07 13:41:13 +0200

    fix table

--- ClientApp/src/views/SavedProjects.vue
+++ ClientApp/src/views/SavedProjects.vue
@@ -1,7 +1,7 @@
 <template>
-  <v-container class="pt-0 pb-0" fluid style="height:100vh">
+  <v-container fluid>
     <v-row>
-      <v-col cols="12" class="pt-0 pb-0">
+      <v-col cols="12">
         <v-data-table
           :headers="headers"
           :items="$store.state.projects.items"
@@ -24,7 +24,9 @@
             </div>
           </template>
           <template v-slot:[`item.actions`]="{ item }">
-            <v-icon @click="deleteItem(item)">mdi-delete</v-icon>
+            <div @click="deleteItem(item)">
+              <v-icon>mdi-delete</v-icon>
+            </div>
           </template>
         </v-data-table>
         <v-dialog v-model="dialog" max-width="500">
@@ -32,8 +34,8 @@
             <v-card-title>Are you sure you want to delete this project?</v-card-title>
             <v-card-actions>
               <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
-              <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+              <v-btn color="secondary darken-1" text @click="closeDelete">Cancel</v-btn>
+              <v-btn color="secondary darken-1" text @click="deleteItemConfirm">Yes</v-btn>
               <v-spacer></v-spacer>
             </v-card-actions>
           </v-card>
@@ -60,7 +62,7 @@ export default Vue.extend({
           sortable: true,
           value: 'name',
         },
-        { text: 'Actions', value: 'actions', sortable: false, align: 'end' },
+        { text: '', value: 'actions', sortable: false, align: 'end' },
       ],
       dialog: false,
       deletableProject: null as (null | Project),
@@ -108,11 +110,17 @@ export default Vue.extend({
 });
 </script>
 
-<style scoped>
-  .projectsTable >>> tbody tr{
+<style>
+  td{
     cursor: pointer;
   }
-  .projectCard{
-    cursor: pointer;
+  td.text-end {
+    width: 10%;
+  }
+  td.text-start:hover{
+    color: #209EF0;
+  }
+  td.text-end:hover i.v-icon.mdi-delete {
+    color: #209EF0;
   }
 </style>
